54951f4a23d7ac89e85b63f9d15ab3f720406614
Ranierelp <118647138+Ranierelp@users.noreply.github.com>
2024-03-19 21:40:08 -0300

    validações tela de registro da clinica

--- user/forms.py
+++ user/forms.py
@@ -1,47 +1,78 @@
 from django import forms      
-from django.forms import Select
 from .models import CustomUser, Clinic, Dentist
-from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
+from . import validations
 
-from django import forms
-from .models import CustomUser, Clinic
-
-class ClinicRegistrationForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['email', 'phone', 'password']
-
-    name_clinic = forms.CharField(label='Nome da Clínica', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Nome da Clínica'}))
-    cnpj = forms.CharField(label='CNPJ', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'CNPJ'}))
-    confirm_password = forms.CharField(label='Confirmar Senha', widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'placeholder': 'Confirmar Senha'}))
+class ClinicRegistrationForm(forms.Form):
+    name_clinic = forms.CharField(
+        label='Nome da Clínica',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Nome da Clínica',
+            'id': 'id_name_clinic'
+        })
+    )
+    cnpj = forms.CharField(
+        label='CNPJ',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'CNPJ'
+        })
+    )
+    email = forms.EmailField(
+        label='Email',
+        validators=[validations.email_unique],
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+    )
+    phone = forms.CharField(
+        label='Telefone',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Telefone'
+        })
+    )
+    password = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Senha'
+        }),
+        validators=[MinLengthValidator(6, message='Senha deve ter no mínimo 6 caracteres')]
+    )
+    confirm_password = forms.CharField(
+        label='Confirmar Senha',
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Confirmar Senha'
+        })
+    )
 
     def clean(self):
         cleaned_data = super().clean()
         password1 = cleaned_data.get('password')
         password2 = cleaned_data.get('confirm_password')
 
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('Senhas não conferem')
+        validations.validate_password_match(password1, password2)
 
         return cleaned_data
 
     def save(self, commit=True):
-        user = CustomUser.objects.create(
+        user = CustomUser.objects.create_user(
             email=self.cleaned_data['email'],
-            phone=self.cleaned_data['phone'],
             password=self.cleaned_data['password'],
-            type_user='C'  # Set type_user here
         )
+        user.type_user = 'C'
+        user.save()
+
         clinic = Clinic.objects.create(
             user=user,
-            name=self.cleaned_data['name_clinic'],
             cnpj=self.cleaned_data['cnpj'],
         )
-        return clinic
 
+        return user
 
         
         
